1c5a285f52b75d6f23a14b41c5c296f9540e854f
André <andrezaocordeiro@gmail.com>
2022-09-30 00:49:11 -0300

    configuração inicial

---
+++ src/main/java/com/apache/apacheestudos/ApacheEstudosApplication.java
@@ -0,0 +1,13 @@
+package com.apache.apacheestudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApacheEstudosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApacheEstudosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/apache/apacheestudos/ApacheEstudosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apache.apacheestudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApacheEstudosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
