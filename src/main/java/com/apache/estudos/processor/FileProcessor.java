266e7dcbcf3e6ca379d37401f7f5a91175ddd5d0
André <andrezaocordeiro@gmail.com>
2022-10-23 22:40:35 -0300

    direct:

---
+++ src/main/java/com/apache/estudos/processor/FileProcessor.java
@@ -0,0 +1,15 @@
+package com.apache.estudos.processor;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.Processor;
+
+import java.io.File;
+
+public class FileProcessor implements  Processor {
+
+
+    @Override
+    public void process(Exchange exchange) throws Exception {
+        System.out.println(exchange.getIn().getBody(File.class));
+    }
+}
--- src/main/java/com/apache/estudos/routes/FileRouter.java
+++ src/main/java/com/apache/estudos/routes/FileRouter.java
@@ -1,15 +1,20 @@
 package com.apache.estudos.routes;
 
+import com.apache.estudos.processor.FileProcessor;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class FileRouter extends RouteBuilder {
 
-    private String path = "";
+
     @Override
     public void configure() throws Exception {
-        from(path).log("Apache camel ${}").to(path);
+        from("direct:log-file").log("log: ${header.CamelFileName}")
+                .process(new FileProcessor());
+
+        from("file:C:\\Users\\André\\Documents\\TesteApache\\input")
+                .to("direct:log-file");
     }
 
 
